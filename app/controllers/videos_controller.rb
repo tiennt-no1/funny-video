098306fd212690e7de5bdef6e66c8cf7383ced86
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-01 13:29:36 +0700

    create videos controller

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,12 @@
+class VideosController < ApplicationController
+  before_action :authorize_request, except: :create
+
+  def index
+    @videos = Video.all
+  end
+
+  def create
+    video = Video.create(youtube_url: params.require(:youtube_url))
+    render json: {success: true, data: video}
+  end
+end
