484bc8eb7fe5d4d9be9e256347d1c49debdb37b2
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-01 13:20:20 +0700

    login method on authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,2 +1,20 @@
 class AuthenticationController < ApplicationController
+
+  def login
+    @user = User.find_by!(username: params[:username])
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: {token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                    username: @user.username}, status: :ok
+    else
+      render json: {error: 'unauthorized'}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:username, :password)
+  end
 end
