39e3083bda7d666eb7b864825025ebfc9c6034aa
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-01 13:32:27 +0700

    add videos to routes, create view on index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :users, param: :_username
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
+
+
+  resources 'videos', only: %i(index create)
+  root to: 'videos#index'
 end
