09b5d549d29b886a6be3ed257ef16fd2a5d17771
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-02 13:42:29 +0700

    render json token and store on cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def not_found
-    render json: { error: 'not_found' }
+    render json: {error: 'not_found'}
   end
 
   def authorize_token
     header = request.headers['Authorization']
-    header.to_s.split(' ').last
+    header.to_s.split(' ').last || cookies['access-token']
   end
 
   def authorize_request
@@ -18,9 +18,15 @@ class ApplicationController < ActionController::Base
       @decoded = JsonWebToken.decode(token)
       @current_user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
-      redirect_to auth_new_path
+      respond_to do |format|
+        format.html { return redirect_to auth_new_path }
+        format.any { render json: {error: 'not_found'}, status: :unauthorized }
+      end
     rescue JWT::DecodeError => e
-      redirect_to auth_new_path
+      respond_to do |format|
+        format.html { return redirect_to auth_new_path }
+        format.any { render json: {error: 'invalid_token', status: :unauthorized} }
+      end
     end
   end
 end
