64a2b370d0a74137f02e43a2c65d96f2d6f4a5ce
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-02 21:12:13 +0700

    add more columns to video

---
+++ db/migrate/20191002140403_add_more_column_to_videos.rb
@@ -0,0 +1,8 @@
+class AddMoreColumnToVideos < ActiveRecord::Migration[5.2]
+  def change
+    add_column :videos, :like, :integer
+    add_column :videos, :dislike, :integer
+    add_column :videos, :title, :string
+    add_column :videos, :desc, :string
+  end
+end
