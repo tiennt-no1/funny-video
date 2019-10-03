f423ea679164ec06a35d56b91d777ab0965a2d87
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-03 10:37:30 +0700

    fix crashes

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,14 +2,14 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do
-    username Faker::Internet.email
-    password "p@ssw0rd"
+    username { Faker::Internet.email }
+    password { "p@ssw0rd" }
   end
 
   factory :video do
-    youtube_url "https://www.youtube.com/watch?v=g1jun_V1o2E"
-    title Faker::Name.name
-    desc Faker::String.random
+    youtube_url { "https://www.youtube.com/watch?v=g1jun_V1o2E" }
+    title { Faker::Name.name }
+    desc { Faker::String.random }
   end
 
   factory :token
