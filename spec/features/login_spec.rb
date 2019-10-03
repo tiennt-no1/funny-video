3b64430bb56bc48e5f6771b0fec6c68fc9e4f3fa
tiennt-no1 <tiennt.june.20@gmail.com>
2019-10-03 19:37:59 +0700

    test invalid login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe 'Register new user', type: :feature, js: true do
   let (:user) { create :user }
   scenario 'valid inputs' do
     visit auth_new_path
+    expect(page).to have_selector("a", text: "Register")
     fill_in 'username', with: user.username
     fill_in 'password', with: user.password
     click_button 'login'
@@ -14,5 +15,18 @@ RSpec.describe 'Register new user', type: :feature, js: true do
     click_button 'logout'
     expect(page).not_to have_selector("button", text: "Share video")
     expect(page).not_to have_selector("button#logout", text: "Log out")
+  end
+
+  scenario 'invalid inputs' do
+    visit auth_new_path
+    fill_in 'password', with: user.password
+    click_button 'login'
+    expect(page).to have_selector('h2.swal2-title', text: 'Invalid username or password')
+
+    visit auth_new_path
+    fill_in 'username', with: user.username
+    fill_in 'password', with: "362187sakhd"
+    click_button 'login'
+    expect(page).to have_selector('h2.swal2-title', text: 'Unauthorized')
   end
 end
